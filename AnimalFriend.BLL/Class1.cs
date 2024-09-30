2066a2e2038ee69a7ee0ee9c4a6636a80b43738d
genady <puhovkostya1@gmail.com>
2024-09-30 22:51:47 +0300

    Shelter card is ready
    add metods getAllSheltor but it need override
    and card page is only looking need

--- AnimalFriend.BLL/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AnimalFriend.BLL
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ AnimalFriend.Core/OutputModels/ShelterCardOutputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFriend.Core.OutputModels
+{
+    public class ShelterCardOutputModel
+    {
+        public int Id { get; set; }
+
+        public string ImagePath { get; set; }
+  
+        public string NameShelter { get; set; }
+
+        public string TypeAnimal { get; set; }
+
+        public string Description { get; set; }
+    }
+}
