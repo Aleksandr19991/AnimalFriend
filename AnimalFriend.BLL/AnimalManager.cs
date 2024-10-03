7c287ed12b768fad22b07096500370c61d3f7ee9
Aleksandr19991 <am79250198173@yandex.ru>
2024-10-04 00:32:18 +0300

    AddFormAmimalCard

--- AnimalFriend.BLL/AnimalManager.cs
+++ AnimalFriend.BLL/AnimalManager.cs
@@ -19,24 +19,30 @@ namespace AnimalFriend.BLL
                     Id=1,
                     Name="Мухтар",
                     AnimalSpecies="Собакен",
-                    Age=1,
-                    Description="Гав"
+                    Age="4 года",
+                    Description="Гав",
+                    NameShelter="Приют1",
+                    CallShelter=+70000000000
                 },
                 new AnimalOutputModel()
                 {
                     Id=2,
                     Name="Барсик",
                     AnimalSpecies="Котэ",
-                    Age=1,
-                    Description="Мяу"
+                    Age="3 года",
+                    Description="Мяу",
+                    NameShelter="Приют2",
+                    CallShelter=+70000000000
                 },
                 new AnimalOutputModel()
                 {
                     Id=3,
                     Name="Беляш",
                     AnimalSpecies="Собакен",
-                    Age=4,
-                    Description="Гав"
+                    Age="5 лет",
+                    Description="Гав",
+                    NameShelter="Приют1",
+                    CallShelter=+70000000000
                 },
 
                 new AnimalOutputModel()
@@ -44,8 +50,10 @@ namespace AnimalFriend.BLL
                     Id=4,
                     Name="Мурка",
                     AnimalSpecies="Котэ",
-                    Age=8,
-                    Description="Мяу"
+                    Age="7 лет",
+                    Description="Мяу",
+                    NameShelter="Приют2",
+                    CallShelter=+70000000000
                 },
                 
                 new AnimalOutputModel()
@@ -53,36 +61,82 @@ namespace AnimalFriend.BLL
                     Id=5,
                     Name="Вольт",
                     AnimalSpecies="Собакен",
-                    Age=10,
-                    Description="Гав"
+                    Age="10 лет",
+                    Description="Гав",
+                    NameShelter="Приют1",
+                    CallShelter=+70000000000
                 },
             };
         }
 
         public AnimalOutputModel GeAnimalById(int id)
         {
-            if (id % 2 == 0)
+            if (id == 1)
+            {
+                return new AnimalOutputModel()
+                {
+                    Id = id,
+                    Name = "Мухтар",
+                    AnimalSpecies = "Собакен",
+                    Age = "4 года",
+                    Description = "Гав",
+                    NameShelter = "Приют1",
+                    CallShelter = +70000000000
+                };
+            }
+            else if (id == 2)
+            {
+                return new AnimalOutputModel()
+                {
+                    Id = id,
+                    Name = "Барсик",
+                    AnimalSpecies = "Котэ",
+                    Age = "3 года",
+                    Description = "Мяу",
+                    NameShelter = "Приют2",
+                    CallShelter = +70000000000
+                };
+            }
+            else if (id == 3)
+            {
+                return new AnimalOutputModel()
+                {
+                    Id = id,
+                    Name = "Беляш",
+                    AnimalSpecies = "Собакен",
+                    Age = "5 лет",
+                    Description = "Гав",
+                    NameShelter = "Приют1",
+                    CallShelter = +70000000000
+                };
+            }
+            else if (id == 4)
             {
                 return new AnimalOutputModel()
                 {
                     Id = id,
                     Name = "Мурка",
                     AnimalSpecies = "Котэ",
-                    Age = 8,
-                    Description = "Мяу"
+                    Age = "7 лет",
+                    Description = "Мяу",
+                    NameShelter = "Приют2",
+                    CallShelter = +70000000000
                 };
             }
-            else
+            else if (id == 5)
             {
                 return new AnimalOutputModel()
                 {
-                    Id = 5,
+                    Id = id,
                     Name = "Вольт",
                     AnimalSpecies = "Собакен",
-                    Age = 10,
-                    Description = "Гав"
+                    Age = "10 лет",
+                    Description = "Гав",
+                    NameShelter = "Приют1",
+                    CallShelter = +70000000000
                 };
             }
+            else { return new AnimalOutputModel(); }
         }
     }
 }
