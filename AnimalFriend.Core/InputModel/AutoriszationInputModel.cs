a5c519b1d512adbd3e963ebfc31d2a1915b76ef5
genady <puhovkostya1@gmail.com>
2024-10-04 18:21:25 +0300

    transferred autintification modal add validation  it

--- AnimalFriend.Core/InputModel/AutoriszationInputModel.cs
+++ AnimalFriend.Core/InputModel/AutoriszationInputModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace AnimalFriend.Core.InputModel
 {
     public class AutoriszationInputModel
     {
+        [Required(ErrorMessage = "enter NAME")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "min char 6")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "enter PASSWORD")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "min char 5")]
         public string Password { get; set; }
     }
 }
