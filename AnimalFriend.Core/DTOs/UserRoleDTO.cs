e49d174a2954cfab25f072e74df9bf0e94a76fc3
Aleksandr19991 <am79250198173@yandex.ru>
2024-10-12 21:29:38 +0300

    Update Models

--- AnimalFriend.Core/DTOs/UserRoleDTO.cs
+++ AnimalFriend.Core/DTOs/UserRoleDTO.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AnimalFriend.BLL.Mappings
+namespace AnimalFriend.Core.DTOs
 {
-    internal class ApplicationForAddingShelterProfile
+    internal class UserRoleDTO
     {
     }
 }
