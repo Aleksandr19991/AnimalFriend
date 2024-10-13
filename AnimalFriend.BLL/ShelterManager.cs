9ba3a2451b5ef3ba8fc399b0f2a75bc6512ff20d
genady <puhovkostya1@gmail.com>
2024-10-13 21:02:18 +0300

    scroll added

--- AnimalFriend.BLL/ShelterManager.cs
+++ AnimalFriend.BLL/ShelterManager.cs
@@ -40,6 +40,38 @@ namespace AnimalFriend.BLL
                     NameShelter = "kittys shalter",
                     Description = "new kittys shelter"
                 },
+                 new ShelterCardOutputModel()
+                {
+                    Id = 1,
+                    ImagePath = "images/pes.jpg",
+                    TypeAnimal = "dog",
+                    NameShelter = "dogs shelter",
+                    Description = "new dogs shelter"
+                },
+                new ShelterCardOutputModel()
+                {
+                    Id = 2,
+                    ImagePath = "images/shenok.jpg",
+                    TypeAnimal = "puppy",
+                    NameShelter = "puppys shelter",
+                    Description = "new puppys shelter"
+                },
+                new ShelterCardOutputModel()
+                {
+                    Id = 3,
+                    ImagePath = "images/cat.jpg",
+                    TypeAnimal = "cat",
+                    NameShelter = "cat shalter",
+                    Description = "new cats shelter"
+                },
+                new ShelterCardOutputModel()
+                {
+                    Id = 4,
+                    ImagePath = "images/kitty.JPEG",
+                    TypeAnimal = "kitty",
+                    NameShelter = "kittys shalter",
+                    Description = "new kittys shelter"
+                },
             };
         }
     }
