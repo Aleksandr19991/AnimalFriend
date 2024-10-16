e2758b6c8417eca6cecc102ae87621b05724bc57
genady <puhovkostya1@gmail.com>
2024-10-16 23:27:21 +0300

    delete AnimalTypeDTO create AnimalTypeDto

--- AnimalFriend.Core/DTOs/AnimalTypeDTO.cs
+++ AnimalFriend.Core/DTOs/AnimalTypeDTO.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace AnimalFriend.Core.DTOs
 {
     public class AnimalTypeDto
     {
-        public int Id {  get; set; }
+        public int Id { get; set; }
 
         public string Type { get; set; }
     }
