7612d8e18c178a804f203f376a6f13e130024cbf
Aleksandr19991 <am79250198173@yandex.ru>
2024-10-18 02:21:59 +0300

    CorrectingMistake

---
+++ AnimalFriend.Core/DTOs/UserRoleDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFriend.Core.DTOs
+{
+    public class UserRoleDto
+    {
+        public int Id { get; set; }
+
+        public string Role { get; set; }
+    }
+}
