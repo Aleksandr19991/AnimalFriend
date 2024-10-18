ac68c9e3fa091b31bb54e39349953bf69d876b74
genady <puhovkostya1@gmail.com>
2024-10-19 01:22:24 +0300

    add  AdminPage and add litle content and form in pages

---
+++ AnimalFriend.Core/InputModel/ApplicationCreateShelterInputModel.cs
@@ -0,0 +1,26 @@
+﻿
+using AnimalFriend.Core.DTOs;
+
+namespace AnimalFriend.Core.InputModel
+{
+    public class ApplicationCreateShelterInputModel
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public bool IsYes { get; set; } = true;
+    }
+}
