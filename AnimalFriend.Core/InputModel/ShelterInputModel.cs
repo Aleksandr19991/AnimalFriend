c7ba59a8fcc26fe1cfc8459d1c37a1bec7f66553
flyaka <flyaka1991@mail.ru>
2024-10-19 19:20:53 +0300

    Creater ShelterInputModel

---
+++ AnimalFriend.Core/InputModel/ShelterInputModel.cs
@@ -0,0 +1,24 @@
+﻿using AnimalFriend.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFriend.Core.InputModel
+{
+    public class ShelterInputModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
