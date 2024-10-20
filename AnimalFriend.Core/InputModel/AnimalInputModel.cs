836bedf4bb522279ebf943994dd731f0dd5d6526
genady <puhovkostya1@gmail.com>
2024-10-20 14:18:18 +0300

    update animalinputmodel

---
+++ AnimalFriend.Core/InputModel/AnimalInputModel.cs
@@ -0,0 +1,21 @@
+﻿namespace AnimalFriend.Core.InputModel
+{
+    public class AnimalInputModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int? Age { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public string? Type { get; set; }
+
+        public bool? IsDeleted { get; set; }
+    }
+}
+
+
