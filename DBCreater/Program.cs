4c524c0686d6e95336b73e5a6c102d304dd9efc2
genady <puhovkostya1@gmail.com>
2024-10-20 15:55:49 +0300

    Get request to sheltor for admin is working

--- DBCreater/Program.cs
+++ DBCreater/Program.cs
@@ -13,7 +13,62 @@ namespace DBCreater
             Context context = new Context();
             //context.Database.EnsureCreated();
 
-     
-        }   
+
+            //var result = context.RequestsForAdd.ToList();
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item.Id);
+            //}
+
+            //var authorize = context.Users.Where(s => s.Email == "admin@gmail.com").FirstOrDefault();
+            //Console.WriteLine(authorize.Id);
+            //var t = authorize.Role;
+            //Console.WriteLine(t.Id);
+
+
+            //var user = context.Users.Where(s => s.Email == "admin@gmail.com").
+            //    Include(b => b.Role).FirstOrDefault();
+            //Console.WriteLine(user.Role.Id);
+
+            //var authorize = context.Users.Where(s => s.Email == "admin@gmail.com").FirstOrDefault();
+            //Console.WriteLine(authorize.Id);
+            //var a = authorize.Role;
+
+            //authorize.Role.Id = context.UserRoles.Where(s => s.Id == 3).FirstOrDefault().Id;
+            //context.SaveChanges();
+
+
+
+
+            //ADD ROLE ADMIN
+            //UserRoleDto role = new();
+            //role.Id = 3;
+            //role.Role = "admin";
+            //var authorize = context.Users.Where(s => s.Email == "admin@gmail.com").FirstOrDefault();
+            //Console.WriteLine(authorize.Email);
+            //authorize.Role = role;
+            //context.SaveChanges();
+
+
+
+            //CREATE USER ROLE
+            //var roleIdA = new UserRoleDto();
+            //roleIdA.Id = 3;
+            //roleIdA.Role = "admin";
+            //context.UserRoles.Add(roleIdA);
+            //context.SaveChanges();
+
+            //var roleIdM = new UserRoleDto();
+            //roleIdM.Id = 2;
+            //roleIdM.Role = "manager";
+            //context.UserRoles.Add(roleIdM);
+            //context.SaveChanges();
+
+            //var roleIdU = new UserRoleDto();
+            //roleIdU.Id = 1;
+            //roleIdU.Role = "user";
+            //context.UserRoles.Add(roleIdU);
+            //context.SaveChanges();
+        }
     }
 }
